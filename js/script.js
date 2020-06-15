6c2041d9efa2ea8086f612bbbb4b39d9fcc5a306
Romnasi <63646879+Romnasi@users.noreply.github.com>
2020-06-16 00:57:48 +0300

    добавляем интерактивности 2
    - локал сторадж для поля "Количество детей"
    - какая-то анимация

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ let quantityAdults = document.querySelector('.field-quantity-adults');
 let quantityChildren = document.querySelector('.field-quantity-children');
 
 let isStorageSupport = true;
-let storageAdults = "";
+let storageAdults = '';
+let storageChildren = '';
 
 try {
   storageAdults = localStorage.getItem('adults');
+  storageChildren = localStorage.getItem('children');
 } catch (err) {
   isStorageSupport = false;
 }
@@ -18,20 +20,29 @@ buttonHide.addEventListener('click', function () {
   mainForm.classList.toggle('hide');
   dateArrival.focus();
 
-  if (storageAdults) {
+  if (storageAdults && storageChildren) {
     quantityAdults.value = storageAdults;
+    quantityChildren.value = storageChildren;
   }
 });
-
-
 
 mainForm.addEventListener('submit', function (evt) {
   if (!dateArrival.value || !dateDeparture.value || !quantityAdults.value || !quantityChildren.value) {
     evt.preventDefault();
+
   } else {
     if (isStorageSupport) {
       localStorage.setItem('adults', quantityAdults.value);
+      localStorage.setItem('children', quantityChildren.value);
     }
   }
-
 });
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (mainForm.classList.contains('hide')) {
+    } else {
+      mainForm.classList.add('hide');
+    }
+  }
+})
